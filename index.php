6782e7b3c244ca2ae370514f099c8bec2ed9a1a2
Walter Sosa <sosa.tizon@gmail.com>
2017-12-04 02:30:08 -0300

    Fix modelo login

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)).DS);
 define('APP_PATH', ROOT . 'app'. DS);
 
-
+try {
    
 require_once APP_PATH . 'Bootstrap.php';
 require_once APP_PATH . 'config.php';
@@ -15,8 +15,8 @@ require_once APP_PATH . 'Model.php';
 require_once APP_PATH . 'View.php';
 require_once APP_PATH . 'Database.php';
 require_once APP_PATH . 'Session.php';
-    
-try {
+Session::init();
+
     Bootstrap::run(new Request());
 } catch (Exception $exc) {
     echo $exc->getMessage();
