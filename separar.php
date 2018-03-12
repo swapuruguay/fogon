1856738087641a8768f044714c2caa0504ed7698
Walter Sosa <sosa.tizon@gmail.com>
2018-03-12 14:08:41 -0300

    Control de apellido

--- separar.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $mysqli = new mysqli('localhost', 'root', 'ws7', 'fogon');
-  $sql = "SELECT nombre FROM socios WHERE apellido IS NULL";
-  $result = $mysqli->query($sql);
-  if($row = $result->mysqli_fetch_object()) {
-    print_r($row);
-  }
-
- ?>
